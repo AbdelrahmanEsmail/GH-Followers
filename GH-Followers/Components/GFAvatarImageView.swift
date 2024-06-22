72172b6770824abe8b33294ce17d7f61fcda980a
Abdelrahman Esmail <pro.abdelrahmanismail@gmail.com>
2024-06-22 12:23:06 +0300

    Configured UICollectionDataSource,, UIHelper

--- GH-Followers/Components/GFAvatarImageView.swift
+++ GH-Followers/Components/GFAvatarImageView.swift
@@ -27,4 +27,16 @@ class GFAvatarImageView: UIImageView {
         translatesAutoresizingMaskIntoConstraints = false
     }
     
+    func downloadImage(from urlString: String){
+        guard let url = URL(string: urlString) else {return}
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if error != nil {return}
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {return}
+            guard let data = data else {return}
+        }
+        
+        task.resume()
+    }
+    
 }
