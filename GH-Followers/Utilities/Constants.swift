e8e6b1c3fe116c2fdf5d136c49ad5529ad44d47e
Abdelrahman Esmail <pro.abdelrahmanismail@gmail.com>
2024-10-09 05:18:35 +0300

    UserInfoVC Refactoring & Created Custom Components

---
+++ GH-Followers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GH-Followers
+//
+//  Created by Abdelrahman Esmail on 26/06/2024.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
